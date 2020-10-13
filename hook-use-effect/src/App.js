f1d7e807e5c4e34f1795f0292eb7853636799e1f
Ben Foord <blfoord@gmail.com>
2020-10-14 09:38:45 +1300

    Untested use effect code

--- hook-use-effect/src/App.js
+++ hook-use-effect/src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState, useEffect} from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    //Define State
+    const [name, setName] = useState({firstName: 'name', surname: 'surname'});
+    const [title, setTitle] = useState('BIO');
+   
+    //Call the use effect hook
+    useEffect(() => {
+      setName({firstName: 'Prapti', surname: 'Mane'})
+      setTitle({'My Full Name'}) //Set Title
+    }, [])// pass in an empty array as a second argument
+    
+    return(
+        <div>
+            <h1>Title: {title}</h1>
+            <h3>Name: {name.firstName}</h3>
+            <h3>Surname: {name.surname}</h3>
+        </div>
+    );
+};
+export default App
